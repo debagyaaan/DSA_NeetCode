58b198d13909056703cf85e3d7cbd92cd2258504
Debagyan Bordoloi <144057544+debagyaaan@users.noreply.github.com>
2025-06-17 01:57:01 +0530

    Update contains_duplicate.cpp

--- Arrays_Hashing/contains_duplicate.cpp
+++ Arrays_Hashing/contains_duplicate.cpp
@@ -14,3 +14,20 @@ public:
        return flag;
     }
 };
+
+//better code
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       unordered_set<int> seen;
+        for (int i:nums) {
+            if(seen.count(i)){
+                return true;
+            } else {
+                seen.insert(i);
+            }
+        }
+        return false;
+    }
+};
